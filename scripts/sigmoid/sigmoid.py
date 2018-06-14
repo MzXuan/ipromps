d9b6e43aff523560ea0a94e24712b60012977e6f
tony <470963660@qq.com>
2018-06-14 14:17:08 +0800

    test_para_update_v6.py

--- scripts/sigmoid/sigmoid.py
+++ scripts/sigmoid/sigmoid.py
@@ -5,46 +5,27 @@ import numpy as np
 
 #sigmoid = lambda x: 1 / (1 + np.exp(-x))
 def sigmoid(x):
-    return (1 / (1 + np.exp(-x)))
+    # return (1 / (1 + np.exp(-x)))
+    i = -3
+    k = 3
+    return (1 / (1 + np.exp(-k*(x-i))))
 
 def sigmoid_1(x):
-    return (1 / (1 + np.exp(x)))
+    i = -3
+    k = 3
+    return (1 / (1 + np.exp(k*(x-i))))
 
 mySamples = []
 mySigmoid = []
 
-# generate an Array with value ???
-# linespace generate an array from start and stop value
-# with requested number of elements. Example 10 elements or 100 elements.
-# 
-x = plt.linspace(-5,5,31)
-y = plt.linspace(-5,5,31)
+
+x = plt.linspace(-5,5,101)
+y = plt.linspace(-5,5,101)
+x_t = np.linspace(0,1,101)
 
 # prepare the plot, associate the color r(ed) or b(lue) and the label 
-plt.plot(x, sigmoid_1(x), 'r', label='linspace(-10,10,10)')
-plt.plot(y, sigmoid(y), 'b', label='linspace(-10,10,100)')
+plt.plot(x_t, sigmoid_1(x), 'r')
+plt.plot(x_t, sigmoid(y), 'b')
 
-# Draw the grid line in background.
-plt.grid()
 
-# Title & Subtitle
-plt.title('Sigmoid Function')
-plt.suptitle('Sigmoid')
-
-# place the legen boc in bottom right of the graph
-plt.legend(loc='lower right')
-
-# write the Sigmoid formula
-plt.text(4, 0.8, r'$\sigma(x)=\frac{1}{1+e^{-x}}$', fontsize=15)
-
-#resize the X and Y axes
-plt.gca().xaxis.set_major_locator(plt.MultipleLocator(1))
-plt.gca().yaxis.set_major_locator(plt.MultipleLocator(0.1))
- 
-
-# plt.plot(x)
-plt.xlabel('X Axis')
-plt.ylabel('Y Axis')
-
-# create the graph
 plt.show()
